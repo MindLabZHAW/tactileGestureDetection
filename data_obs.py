69465e5ddba33b9a766de098e0a1996824042690
DavidSong2000 <pipiluyishi@163.com>
2024-07-09 16:09:50 +0200

    data fetching without saving

--- data_obs.py
+++ data_obs.py
@@ -47,48 +47,16 @@ from threading import Event
 import csv
 import datetime
 import os
-from threading import Event
 
 def print_robot_state(data):
     # rospy.loginfo(np.array(data.q_d) - np.array(data.q))
-    print(data.q)
-
-# def move_robot(fa:FrankaArm, event: Event):
-
-# 	joints = pd.read_csv(joints_data_path)
-
-# 	# preprocessing
-# 	joints = joints.iloc[:, 1:8]
-# 	joints.iloc[:,6] -= np.deg2rad(45) 
-# 	print(joints.head(5), '\n\n')
-# 	fa.goto_joints(np.array(joints.iloc[0]),ignore_virtual_walls=True)
-# 	fa.goto_gripper(0.02)
-	
-# 	while True:	
-# 		try:	
-# 			for i in range(joints.shape[0]):
-# 				fa.goto_joints(np.array(joints.iloc[i]),ignore_virtual_walls=True,duration=4)
-# 				#time.sleep(0.01)
-
-# 		except Exception as e:
-# 			print(e)
-# 			event.set()
-# 			break
-	
-# 	print('fininshed .... !')
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.q)
+    # print(data.dq)
+    print(data.elbow)
 
 if __name__ == '__main__':
     # create FrankaArm instance
-    event = Event()
     fa = FrankaArm()
-    
-    # # create folder
-    # folder_name = input('Enter tag name: ')
-
-    # datapath = 'Obs_DATA/' + folder_name
-
-    # os.makedirs(datapath)
-    # print("Directory '%s' created" % datapath)
 
     rospy.Subscriber(name="/robot_state_publisher_node_1/robot_state", data_class=RobotState, callback=print_robot_state, queue_size=1)
-    # move_robot(fa, event)
+    rospy.spin()
