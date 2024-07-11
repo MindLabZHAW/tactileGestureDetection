164f3fb967189aff52c42e5dd3e79860138ee813
weimin3 <bonjour_world@126.com>
2024-07-11 13:27:26 +0200

    add visualization file

---
+++ visualization.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import sys
+
+#csv_file_path = "data/0711/20240711_113150.csv"
+csv_file_path = sys.argv[1]
+data = pd.read_csv(csv_file_path)
+
+
+data["timestamp"] = pd.to_datetime(data["timestamp"])
+
+attributes = data["attribute_name"].unique()
+
+print(f"attributes in data: {attributes}")
+
+
+# Create a subfolder for plots
+plot_dir = os.path.splitext(csv_file_path)[0]
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
+
+
+# function to plot waveform for a given attribute
+def plot_waveform(attribute_name):
+    attribute_data = data[data["attribute_name"] == attribute_name]
+
+    if attribute_data.empty:
+        print(f"No data found for attribute: {attribute_name}")
+        return
+    
+    plt.plot(attribute_data["timestamp"],attribute_data[values])
+    plt.title(f"Waveform of {attribute_name}")
+    plt.xlabel("Timestamp")
+    plt.ylabel("Values")
+    plt.show()
+
+    #save the plot
+    plot_path = os.path:join(plot_dir,f"{attribute_name}_waveform.png")
+    plt.savefig(plot_path)
+    plt.close()
+
+
+
+for attribute in attributes:
+    plot_waveform(attribute)
