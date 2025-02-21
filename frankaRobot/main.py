8bf46f496aca38bad9d3b32f43161e056b0d83cd
weimin3 <bonjour_world@126.com>
2025-02-21 14:21:57 +0100

    modified:   Process_Data/1_rawData2LabeledData.ipynb 	renamed:    Process_Data/4_ModelLabelMatch_Visualization.ipynb -> Process_Data/4_1ModelLabelMatch_Visualization.ipynb 	modified:   Process_Data/4_2_ModelPerfomance_singleFolder.ipynb 	new file:   Process_Data/4_3_OverallModelPerformanceVisualization.ipynb 	modified:   frankaRobot/main.py

--- frankaRobot/main.py
+++ frankaRobot/main.py
@@ -82,9 +82,9 @@ features_num = 4
 classes_num = 4
 Normalization = False
 # Model Selection Parameters and path (Please always change them together)
-method = 'TCNN'
-model_path_relative = os.path.join("AIModels/TrainedModels", "1L3DTCNN_02_19_2025_11-30-14Pose123ES36.pth") # relative path from AIModels
-type_network = '1L3DTCNN'
+method = 'RNN'
+model_path_relative = os.path.join("AIModels/TrainedModels", "LSTM_02_18_2025_18-05-54Pose123ES57.pth") # relative path from AIModels
+type_network = 'LSTM'
 # MultiClassifier Parameters and path
 MultiClassifier = False
 if MultiClassifier:
